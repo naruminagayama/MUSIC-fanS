2738c58ee7e229e7e7cddb99107f17f2e6352b31
vagrant <k016a1001@gmail.com>
2020-08-12 11:03:02 +0000

    Delete customer_community_model

--- app/models/customer_community.rb
+++
@@ -1,2 +0,0 @@
-class CustomerCommunity < ApplicationRecord
-end
--- db/migrate/20200802061958_create_customer_communities.rb
+++
@@ -1,8 +0,0 @@
-class CreateCustomerCommunities < ActiveRecord::Migration[5.2]
-  def change
-    create_table :customer_communities do |t|
-
-      t.timestamps
-    end
-  end
-end
