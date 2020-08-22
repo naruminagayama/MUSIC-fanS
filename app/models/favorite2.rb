c09a2bd0df573570ccfab100762bc5c99ca82e96
vagrant <k016a1001@gmail.com>
2020-08-22 07:03:26 +0000

    Add favorite2_log_conf

--- app/models/favorite2.rb
+++ app/models/favorite2.rb
@@ -3,4 +3,10 @@ class Favorite2 < ApplicationRecord
   belongs_to :customer
   belongs_to :song
 
+  def self.save_from_yaml!(yaml_path)
+  	yaml = YAML.load(yaml_path)
+  	favorite2 = self.new
+  	favorite2.save!
+  end
+
 end
