e11a74cc31ce99be0bfff273c15e479779be4460
vagrant <k016a1001@gmail.com>
2020-08-23 20:53:05 +0000

    Fix favorite2_log_conf

--- app/controllers/public/favorite2s_controller.rb
+++ app/controllers/public/favorite2s_controller.rb
@@ -11,10 +11,8 @@ class Public::Favorite2sController < ApplicationController
         format.js { flash.now[:alert] = '登録に失敗しました' }
       end
       logger.error e
-      return render 'public/songs/favorite'
     end
 
-    favorite2.save
     respond_to do |format| 
       format.js { flash.now[:notice] = 'お気に入りに登録しました' }
     end
@@ -31,10 +29,8 @@ class Public::Favorite2sController < ApplicationController
         format.js { flash.now[:alert] = '登録に失敗しました' }
       end
       logger.error e
-      return render 'public/songs/favorite'
     end
 
-    favorite2.destroy
     respond_to do |format| 
       format.js { flash.now[:notice] = 'お気に入りを解除しました' }
     end
