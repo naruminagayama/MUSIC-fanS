2d9b69179b5678b0227e2b26705bcb9c85e05a2f
vagrant <k016a1001@gmail.com>
2020-08-31 11:22:08 +0000

    Add 404_500_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,31 @@ class ApplicationController < ActionController::Base
   	root_path
   end
 
+  unless Rails.env.production?
+    rescue_from Exception,                      with: :render_500
+    rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+  end
+ 
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
   protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana,
                                                          :first_name_kana, :nickname, :image_id])
     end
+
+  private
+ 
+  def render_404
+    render 'error/404', status: :not_found
+  end
+ 
+  def render_500
+    render 'error/500', status: :internal_server_error
+  end
 
 end
