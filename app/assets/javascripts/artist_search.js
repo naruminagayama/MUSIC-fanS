e723e5a64a612c8914c66f17c5aaa1d305168e00
vagrant <k016a1001@gmail.com>
2020-09-01 10:26:24 +0000

    Fix artist_search.js

--- app/assets/javascripts/artist_search.js
+++ app/assets/javascripts/artist_search.js
@@ -10,7 +10,7 @@ $(function() {
      .done(function(artists) {
      $(".contents").empty();
  
-      if (artists.length !== 0) {         //artistsの数が0でない時
+      if (artists.length !== 0) {         //artistsの数が0でない時、
         artists.forEach(function(artist){ //forEachメソッド
           appendArtist(artist);
         });
@@ -25,11 +25,32 @@ $(function() {
   });
 });
 
+
+
   var search_list = $(".contents.row");
+
+
+
   function appendArtist(artist) {
-    var html = `<div><span style="font-size: 100px; color: red;">${artist.name}</span></div>`
+
+    var html = `
+      <div style="padding: 15px; margin: 15px 0 0 0; background: linear-gradient(90deg, rgba(22, 135, 237, 0.6), rgba(20, 55, 90, 0.9));">
+        <div class="flex">
+          <div><img src="${artist.image_id}"></div>
+          <div>
+            <span style="font-size: 30px;">
+              <a href="/front/artists/${artist.id}" style="color: gold; text-decoration: none;">${artist.name}</a>
+            </span>
+            <br><br>
+            <span style="color: white;">${artist.detail}</span>
+          </div>
+        </div>
+      </div>
+    `
+
     $(".contents").append(html);
   }
+
   function appendErrMsgToHTML(msg) {
     var html = `<div class='name'>${ msg }</div>`
     search_list.append(html);
