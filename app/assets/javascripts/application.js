e2c6aaaa4971bbe38f33fbc98cdee15e921c72b2
vagrant <k016a1001@gmail.com>
2020-09-04 21:45:03 +0000

    Fix aboutpage_layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,16 @@
 
 //= require audiojs
 //= require toastr
+
+$(function(){
+  $(window).scroll(function (){
+    $('.fadein').each(function(){
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 200){
+        $(this).addClass('scrollin');
+       }
+    });
+  });
+});
