618a48edf136eb21c11b4b20ee118dadeeaa4182
vagrant <k016a1001@gmail.com>
2020-09-07 17:31:59 +0000

    Fix write_code

--- app/controllers/front/relationships_controller.rb
+++ app/controllers/front/relationships_controller.rb
@@ -1,14 +1,27 @@
 class Front::RelationshipsController < ApplicationController
 
   def follow
-    current_customer.follow(params[:id])
+    begin
+      current_customer.follow(params[:id])
+    rescue => e
+      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+      logger.error e
+      return redirect_to request.referer
+    end
+
     flash[:notice] = 'ユーザーをフォローしました'
     redirect_to request.referer
-
   end
 
   def unfollow
-    current_customer.unfollow(params[:id])
+    begin
+      current_customer.unfollow(params[:id])
+    rescue => e
+      flash.now[:alert] = 'フォローの解除に失敗しました'
+      logger.error e
+      return redirect_to request.referer
+    end
+
     flash[:notice] = 'フォローを解除しました'
     redirect_to request.referer
   end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -27,24 +27,12 @@ class Customer < ApplicationRecord
 
   # ユーザーをフォローする
   def follow(customer_id)
-    begin
-      follower.create!(followed_id: customer_id)
-    rescue => e
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      logger.error e
-      return redirect_to request.referer
-    end
+    follower.create(followed_id: customer_id)
   end
 
   # ユーザーのフォローを外す
   def unfollow(customer_id)
-    begin
-      follower.find_by(followed_id: customer_id).destroy!
-    rescue => e
-      flash.now[:alert] = 'フォローの解除に失敗しました'
-      logger.error e
-      return redirect_to request.referer
-    end
+    follower.find_by(followed_id: customer_id).destroy
   end
 
   # フォローしていればtrueを返す
