f3cfb61d8d24785a368166f997adb8da46c77a81
vagrant <k016a1001@gmail.com>
2020-09-17 15:59:15 +0000

    Fix unique_favorite

--- app/models/favorite_artist.rb
+++ app/models/favorite_artist.rb
@@ -2,5 +2,6 @@ class FavoriteArtist < ApplicationRecord
 
   belongs_to :customer
   belongs_to :artist
+  validates :artist_id, uniqueness: { scope: :customer_id }
 
 end
--- app/models/favorite_song.rb
+++ app/models/favorite_song.rb
@@ -2,5 +2,6 @@ class FavoriteSong < ApplicationRecord
 
   belongs_to :customer
   belongs_to :song
+  validates :song_id, uniqueness: { scope: :customer_id }
 
 end
