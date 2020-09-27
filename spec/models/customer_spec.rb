4cb94e46d953da0cc31e6407464d3ea5af371cda
vagrant <k016a1001@gmail.com>
2020-09-27 17:21:48 +0000

    Fix test_validates

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,40 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
-  context "データが正しく保存される" do
+RSpec.describe 'バリデーション', type: :model do
+  context "必須項目が全て埋まっている時" do
     before do
       @customer = Customer.new
-      @customer.last_name = "田中"
-      @customer.first_name = "二郎"
-      @customer.last_name_kana = "タナカ"
-      @customer.first_name_kana = "ジロウ"
-      @customer.email = "test@example.com"
-      @customer.nickname = "テスト"
-      @customer.password = "password"
-      @customer.password_confirmation = "password"
-      @customer.save
+      @customer.last_name = '田中'
+      @customer.first_name = '二郎'
+      @customer.last_name_kana = 'タナカ'
+      @customer.first_name_kana = 'ジロウ'
+      @customer.email = 'test@example.com'
+      @customer.nickname = 'テスト'
+      @customer.password = 'password'
+      @customer.password_confirmation = 'password'
     end
-    it "全て入力してあるので保存される" do
+    example 'バリデーションに成功する' do
       expect(@customer).to be_valid
     end
   end
 
-  context "データが正しく保存されない" do
+  context '必須項目が全て埋まっていない時' do
     before do
       @customer = Customer.new
-      @customer.last_name = ""
-      @customer.first_name = "二郎"
-      @customer.last_name_kana = "タナカ"
-      @customer.first_name_kana = "ジロウ"
-      @customer.email = "test@example.com"
-      @customer.nickname = "テスト"
-      @customer.password = "password"
-      @customer.password_confirmation = "password"
-      @customer.save
+      @customer.last_name = ''
+      @customer.first_name = '二郎'
+      @customer.last_name_kana = 'タナカ'
+      @customer.first_name_kana = 'ジロウ'
+      @customer.email = 'test@example.com'
+      @customer.nickname = 'テスト'
+      @customer.password = 'password'
+      @customer.password_confirmation = 'password'
     end
-    it "last_nameが入力されていないので保存されない" do
+    example 'バリデーションに失敗する' do
       expect(@customer).to be_invalid
-      expect(@customer.errors[:last_name]).to include("が入力されていません")
+      expect(@customer.errors[:last_name]).to include('が入力されていません')
     end
   end
 end
